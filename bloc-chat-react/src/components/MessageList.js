260380b638cf8bf252eaa3bcfd0f87e7bbc14c74
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-08-09 14:49:10 -0500

    send messages checkpoint

--- bloc-chat-react/src/components/MessageList.js
+++ bloc-chat-react/src/components/MessageList.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 
 class MessageList extends Component {
   constructor(props) {
   super(props);
   this.state = {
   messages: [],
+  newMessages: ''
   };
  this.messagesRef = this.props.firebase.database().ref('messages');
- this.handleChange = this.handleChange.bind(this);
+ this.createMessage = this.createMessage.bind(this);
 }
 
 componentDidMount() {
@@ -19,13 +21,32 @@ componentDidMount() {
 
 }
 
-handleChange(e) {
-     this.setState({ message: e.target.value });
-             }
+createMessage(e){
+     e.preventDefault();
+      if (!this.props.activeRoom || !this.state.messages) { return }
+     this.setState({ messages: [...this.state.newMessages], newMessages: '' });
+     this.messagesRef.push({
+       content: this.state.newMessages,
+       sentAt: firebase.database.ServerValue.TIMESTAMP,
+       username: this.props.user.displayName || "guest",
+       roomId: this.props.activeRoom.key
+     });
+   }
+
+handleMessageChange(e) {
+     this.setState({ newMessages: e.target.value});
+    }
 
   render() {
     return (
       <section className="messages">
+      <div className="create-message">
+    <h1>Write your message!</h1>
+    <form onSubmit={this.createMessage}>
+      <input type="text" value={this.state.newMessages} onChange={ (e) => this.handleMessageChange(e) } />
+      <input type="submit" id="submit" name="submission" />
+    </form>
+    </div>
       <div id='messageList'>
          <ul>
           {this.state.messages.filter(message => message.roomId === this.props.activeRoom.key).map((message, index) =>
--- bloc-chat-react/src/components/User/User.js
+++ bloc-chat-react/src/components/User/User.js
@@ -5,8 +5,19 @@ class User extends Component {
     super(props);
     this.state = {
     userSignIn: false,
+    guestSignIn: false
   };
 }
+
+  signInNoUser() {
+  this.props.firebase.auth().signInAnonymously();
+  this.setState ({ guestSignIn: true });
+  }
+
+  signOutNoUser(){
+  this.props.firebase.auth().signOut();
+  this.setState ({ guestSignIn: false });
+  }
 
   signInWithPopup() {
   const provider = new this.props.firebase.auth.GoogleAuthProvider();
@@ -19,16 +30,16 @@ class User extends Component {
   this.setState ({ userSignIn: false });
     }
 
-  componentDidMount() {
-    this.props.firebase.auth().onAuthStateChanged( user => {
-      this.props.setUser(user);
+    componentDidMount() {
+      this.props.firebase.auth().onAuthStateChanged( user => {
+        this.props.setUser(user);
       })
-  }
+    }
 
    render() {
       return <div className='signIn'>
           {this.state.userSignIn ? <button onClick={() => this.signOutWithPopup()}>Sign out</button> : <button onClick={() => this.signInWithPopup()}>Sign in</button>}
-          {this.props.user ? this.props.user.displayName : <h3>Guest User</h3>}
+          {this.state.guestSignIn ? <button onClick={() => this.signOutNoUser()}>Guest Sign Out</button> : <button onClick={() => this.signInNoUser()}>Guest Sign in</button>}
         </div>
       }
 }
